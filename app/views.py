ce2f81da6285a4b6a26084cbfcb1daacc700bf92
isabele <isabele.maciel.132@gmail.com>
2025-05-30 19:38:47 -0400

    crud author

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Item, Category
-from .forms import ItemForm, CategoryForm
+from .models import Item, Category, Author
+from .forms import ItemForm, CategoryForm, AuthorForm
 
 
 def item_list(request):
@@ -68,3 +68,36 @@ def category_delete(request, pk):
         category.delete()
         return redirect('category_list')
     return render(request, 'app/category_confirm_delete.html', {'category': category})
+
+
+def author_list(request):
+    authors = Author.objects.all()
+    return render(request, 'app/author_list.html', {'authors': authors})
+
+def author_create(request):
+    if request.method == 'POST':
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('author_list')
+    else:
+        form = AuthorForm()
+    return render(request, 'app/author_form.html', {'form': form})
+
+def author_update(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    if request.method == 'POST':
+        form = AuthorForm(request.POST, instance=author)
+        if form.is_valid():
+            form.save()
+            return redirect('author_list')
+    else:
+        form = AuthorForm(instance=author)
+    return render(request, 'app/author_form.html', {'form': form})
+
+def author_delete(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    if request.method == 'POST':
+        author.delete()
+        return redirect('author_list')
+    return render(request, 'app/author_confirm_delete.html', {'author': author})
